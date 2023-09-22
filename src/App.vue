3f19d509182afaabb9cd6f4024e623757c33b7da
Stiven Cardona <stiven.cardona.0924@gmail.com>
2023-09-22 02:52:57 -0500

    Flowbite

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
 
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
 </script>
 
 <template>
-  <header>
-
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-
-  </header>
-
   <RouterView />
 </template>
+
 
