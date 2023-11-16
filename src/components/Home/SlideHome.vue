8b443555e34ade6efa43205d5283b2f7230bb7f0
ruisi <alexisa.ruizq@uqvirtual.edu.co>
2023-11-15 20:42:21 -0500

    se inteto

--- src/components/Home/SlideHome.vue
+++ src/components/Home/SlideHome.vue
@@ -53,9 +53,9 @@
 
 const images = [
     '/src/assets/Images/Carousel1.jpg',
-    '/src/assets/Images/Carousel2.jpg',
-    '/src/assets/Images/Carousel3.jpg',
-    '/src/assets/Images/Carousel4.jpg',
+    '/src/assets/Images/carousel2.jpg',
+    '/src/assets/Images/carousel3.jpg',
+    '/src/assets/Images/carousel4.jpg',
 ]
 
 </script>
